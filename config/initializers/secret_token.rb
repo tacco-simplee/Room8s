3ccff10f7d35b7636a1858b4576c32773200e0cd
tacco <tacco@simplee.com>
2016-09-27 11:12:21 +0300

    first-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Room8s::Application.config.secret_token = 'ea611cdf99686c43cfd5fab4060b3551778e03988a43d1ef8bbb4d2708d59210f3ee50c9a526d980c3b94e2859e41a3d31576191c70118b10d884bd4d8f566b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Room8s::Application.config.session_store :cookie_store, key: '_Room8s_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Room8s::Application.config.session_store :active_record_store
